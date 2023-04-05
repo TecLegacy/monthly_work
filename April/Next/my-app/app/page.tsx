aa3184e801a915d095a6c28b1a977adb6ce11509
TecLegacy <kshvkumar.kk2@gmail.com>
2023-04-05 12:17:51 +0530

    Redux-Redux/toolkit

--- April/Next/my-app/app/page.tsx
+++ April/Next/my-app/app/page.tsx
@@ -1,12 +1,12 @@
-import { Inter } from 'next/font/google';
-import styles from './page.module.css';
+// import { Inter } from 'next/font/google';
+// import styles from './page.module.css';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className='fixed bottom-0 right-0'>Hesllo World </h1>
+    <main>
+      <h1 className=' mt-20 flex items-center justify-center'>Hello World</h1>
     </main>
   );
 }
