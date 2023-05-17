373de2d08eec848373e6065b3b9e19da4b59a976
TecLegacy <kshvkumar.kk2@gmail.com>
2023-05-17 13:35:17 +0530

    Cart add & remove item logic

--- may/react/cart-js/src/store/store.js
+++ may/react/cart-js/src/store/store.js
@@ -1,54 +1,48 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const cartState = {
-  product: [
-    {
-      id: 1,
-      item: 'm',
-      price: 3.0,
-      perItem: 2.0,
-    },
-  ],
-  totalQantity: '',
+  product: [],
 };
 
 const cartSlice = createSlice({
   name: 'Cart',
   initialState: cartState,
-  // reducers: {
-  //   addToCart(state, action) {
-  //     const sentProduct = action.payload;
-  //     /**
-  //      * product {item, price, perItem perice, productquantity
-  //      * }
-  //      * totalQuantity - product length
-  //      */
-  //     //  const newCart =
-  //     // console.log(sentProduct);
-  //     const itemExist = state.product.filter(
-  //       item => item.id === sentProduct.id
-  //     );
-  //     // console.log(state.product);
-  //     console.log('value', itemExist);
-  //     // console.log(sentProduct);
-  //     // const newItem = sentProduct.map(curr => ({
-  //     //   id: sentProduct.id,
-  //     //   item: sentProduct.item,
-  //     //   price: sentProduct.price,
-  //     //   perItem: sentProduct.perItem,
-  //     // }));
+  reducers: {
+    addToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.product.find(item => item.id === newItem.id);
 
-  //     const newItem = [];
-  //     newItem.push({
-  //       id: sentProduct.id,
-  //       item: sentProduct.item,
-  //       price: sentProduct.price,
-  //       perItem: sentProduct.perItem,
-  //     });
-  //     console.log(newItem);
-  //   },
-  //   removeFromCart() {},
-  // },
+      if (!existingItem) {
+        state.product.push({
+          item: newItem.item,
+          price: newItem.price,
+          quantity: 1,
+          perItem: newItem.perItem,
+          id: newItem.id,
+        });
+        console.log('Added to cart');
+      } else {
+        existingItem.quantity++,
+          (existingItem.price = existingItem.price + newItem.perItem);
+      }
+    },
+    removeFromCart(state, action) {
+      const ourItem = action.payload;
+      const remove = state.product.find(item => item.id === ourItem.id);
+
+      if (remove.quantity === 1) {
+        state.product = state.product.filter(item => item.id !== remove.id);
+        console.log('No item Found');
+      } else {
+        remove.quantity--;
+        remove.price -= ourItem.perItem;
+      }
+
+      // let sum = 0;  total amount in cart
+      // state.product.forEach(item => (sum += item.price));
+      // console.log(sum);
+    },
+  },
 });
 
 export const store = configureStore({
