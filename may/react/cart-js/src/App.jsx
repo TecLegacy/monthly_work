25684780c0a197867e6d488949cd08392b909858
TecLegacy <kshvkumar.kk2@gmail.com>
2023-05-18 23:13:11 +0530

    firebase and reduxtoolkit

--- may/react/cart-js/src/App.jsx
+++ may/react/cart-js/src/App.jsx
@@ -1,12 +1,68 @@
 import Cart from '@/components/Cart';
 import { useSelector } from 'react-redux';
 import Notification from '@/ui/notification';
+import { useEffect, useState } from 'react';
 
 function App() {
   const product = useSelector(state => state.cart.product);
+  const cart = useSelector(state => state.cart);
+  const [fireData, setFireData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // FireBase Realtime Database
+  useEffect(() => {
+    const sendCart = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          'https://cart-npm-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+          {
+            method: 'PUT',
+            body: JSON.stringify(cart),
+          }
+        );
+        if (!response.ok) {
+          console.log('No Response found!');
+          setError('error');
+          throw new Error('Something went wrong in Response');
+        }
+        const data = response.json();
+
+        setFireData(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        throw new Error('Something went wrong');
+      }
+    };
+    const timer = setTimeout(() => {
+      sendCart()
+        .then(() => {
+          console.log('Cart sent successfully');
+        })
+        .catch(error => {
+          setError('error');
+          console.log(' Error in senCart API', error);
+          throw new Error(error);
+        });
+    }, 2000);
+
+    return () => {
+      console.log('cleanUp');
+      clearTimeout(timer);
+    };
+  }, [cart]);
+
+  console.log(fireData, 'fireState', 'loading', loading, 'error', error);
+
   return (
     <>
-      <Notification message={'success'} />
+      {/* {loading && } */}
+      <Notification
+        message={loading ? (loading ? 'sending' : 'error') : 'success'}
+      />
+
       <nav className=' flex  w-5/6 mx-auto justify-between items-center bg-transparent '>
         <h1>Redux Toolkit Cart</h1>
 
