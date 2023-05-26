59c793ac2170d623112c5803b8dd24b83d13a5bd
TecLegacy <kshvkumar.kk2@gmail.com>
2023-05-27 01:37:21 +0530

    All posts and cache validation

--- may/react/cart-js/src/components/Posts/index.jsx
+++ may/react/cart-js/src/components/Posts/index.jsx
@@ -1,62 +1,57 @@
 import { useGetPostQuery, useGetAllPostsQuery } from '@/features/apis/apiSlice';
 
 const Posts = () => {
-  const { data: singlePost = [], isLoading } = useGetPostQuery(1);
-  const { data: allPost = [], isLoadings } = useGetAllPostsQuery();
-  console.log('isLoading', isLoading);
-  console.log('isLoadings', isLoadings);
-
-  const posts = allPost?.posts;
-  if (!isLoading) {
-    console.log('x', singlePost);
-    console.log('x', posts);
-  }
+  const { data: singlePostData = [], isLoading } = useGetPostQuery(1);
+  const { data: allPostData = [], isLoadingAllPosts } = useGetAllPostsQuery();
 
   let content;
   if (isLoading) {
-    content = (
-      <h1>
-        <p>Loading...</p>
-      </h1>
-    );
+    content = <h1>Loading...</h1>;
   } else {
-    content = (
-      <h1>
-        <p>Data All loaded</p>
-      </h1>
-    );
+    content = <h1>Data All loaded</h1>;
   }
+
+  // const tagStructure = tags => tags.join(' ');
+
   return (
     <>
       {content}
-      <div className=' mx-auto w-5/6 bg-white p-4  rounded-xs '>
-        <div className=' w-3/4   mx-auto flex  rounded-sm items-center justify-between gap-8 p-4'>
-          <div className=' text-white rounded-sm   bg-slate-400 font-semibold  w-2/5 flex justify-center items-center'>
-            {/* {isLoading} */}
-            <div className=' flex gap-4 flex-col items-center'>
-              <h1 className=' text-lg mt-4'> All Posts</h1>
+      <div className='mx-auto w-5/6 bg-white p-4 rounded-xs'>
+        <div className='w-3/4 mx-auto flex rounded-sm items-center justify-between gap-8 p-4'>
+          <div className='text-white rounded-sm bg-slate-400 font-semibold w-2/5 flex justify-center items-center'>
+            <div className='flex gap-4 flex-col items-center'>
+              <h1 className='text-lg mt-4'>All Posts</h1>
               <div>
-                {posts?.map(item => (
+                {allPostData?.posts?.map(item => (
                   <div
                     key={item.id}
-                    className=' flex items-center gap-4 mb-8 py-4 px-4  flex-col justify-center bg-cyan-50 text-blue-400 w-5/6 mx-auto rounded-md '
+                    className='flex items-center gap-4 mb-8 py-4 px-4 flex-col justify-center bg-cyan-50 text-blue-400 w-5/6 mx-auto rounded-md'
                   >
-                    <h2 className=' text-center'>{item.title}</h2>
+                    <h2 className='text-center'>{item.title}</h2>
                     <div>
-                      <hr className=' w-full bg-slate-300' />
-                      <p className=' text-justify text-sm  font-light font-serif p-1 '>
+                      <hr className='w-full bg-slate-300' />
+                      <p className='text-justify text-sm font-light font-serif p-1'>
                         {item.body}
                       </p>
+                      <details>
+                        {item?.tags?.map((tag, index) => (
+                          <div
+                            key={index}
+                            className='inline-flex items-center px-3 py-1 m-2 bg-gray-200 rounded-full text-sm font-medium text-gray-700'
+                          >
+                            {tag}
+                          </div>
+                        ))}
+                      </details>
                     </div>
                   </div>
                 ))}
               </div>
             </div>
           </div>
-          <div className=' text-white bg-slate-400 font-semibold  w-2/5 flex justify-center'>
-            {/* {isLoading} */}
-            <div className=' flex gap-4 flex-col'>
-              <h1 className=' text-lg'> Single</h1>
+          <div className='text-white bg-slate-400 font-semibold w-2/5 flex justify-center'>
+            <div className='flex gap-4 flex-col'>
+              <h1 className='text-lg'>Single</h1>
               <div className=''>test</div>
             </div>
           </div>
