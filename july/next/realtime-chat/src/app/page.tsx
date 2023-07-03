db775485d1e6cec9c9399b139e5f5836e2d854cb
TecLegacy <kshvkumar.kk2@gmail.com>
2023-07-03 16:40:33 +0530

    Redis installation with latest version and debugged localEnv

--- july/next/realtime-chat/src/app/page.tsx
+++ july/next/realtime-chat/src/app/page.tsx
@@ -1,26 +1,14 @@
-// 'use client';
 import Button from '@/components/ui/Button';
 import { redis } from '@/lib/db';
 
-export default function Home() {
-  const check = async () => {
-    const data = await redis.set('foo', 'bar');
-    console.log('data read', data);
-  };
-
-  // await redis.set('foox', 'bar');
+export default async function Home() {
+  await redis.set('redis', 'installation');
   return (
     <>
       <Button variant={'default'} size={'lg'} isLoading={false}>
         click
       </Button>
       <hr />
-      <div>
-        redis installations
-        <div>
-          <button onClick={() => check()}>fetch </button>
-        </div>
-      </div>
     </>
   );
 }
