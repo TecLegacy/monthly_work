cd362ccb22361c22f18c81b2a0d5147ab78ee1d5
TecLegacy <kshvkumar.kk2@gmail.com>
2023-07-09 15:42:15 +0530

    form validation using zod

--- july/next/realtime-chat/src/components/AddFriendButton.tsx
+++ july/next/realtime-chat/src/components/AddFriendButton.tsx
@@ -1,12 +1,43 @@
 import { FC } from 'react';
 import Button from './ui/Button';
+import { set, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import z from 'zod';
+import { emailValidation } from '@/lib/add-friend';
+import axios, { AxiosError } from 'axios';
 
-interface AddFriendButtonProps {}
+type FormValue = z.infer<typeof emailValidation>;
 
-const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
+const AddFriendButton: FC = ({}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<FormValue>({ resolver: zodResolver(emailValidation) });
+
+  //Add Friend
+  async function addFriend(email: string) {
+    const verifiedEmail = emailValidation.parse(email);
+
+    try {
+      axios.post('/api/friends/add');
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        setError('email', { message: 'Failed Request' });
+        return;
+      }
+
+      if (err instanceof z.ZodError) {
+        setError('email', { message: 'Failed Validation' });
+        return;
+      }
+    }
+  }
+
   return (
     <>
-      <form action='' className='max-w-sm'>
+      <form onSubmit={handleSubmit(onSubmit)} className='max-w-sm'>
         <label
           htmlFor='email'
           className='block text-sm font-medium leading-6 text-gray-900'
@@ -15,6 +46,7 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
         </label>
         <div className='mt-2 flex gap-4'>
           <input
+            {...register('email')}
             placeholder='your@email.com'
             type='text'
             className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
---
+++ july/next/realtime-chat/src/lib/add-friend.ts
@@ -0,0 +1,4 @@
+import { z } from 'zod';
+export const emailValidation = z.object({
+    email: z.string().email(),
+  });
