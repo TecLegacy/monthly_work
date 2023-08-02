b4f92e1d40fc3ba5f24d81f64ef0571a001f3c92
TecLegacy <kshvkumar.kk2@gmail.com>
2023-08-02 15:17:30 +0530

    typesafe reuseable button and animation

---
+++ Aug/Next/Realtime/realtime-chat/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Button from '@/components/Button';
+
+export default function Home() {
+  return (
+    <>
+      <h1>React Real time chat app</h1>
+      <Button isLoading={true}>Click me </Button>
+    </>
+  );
+}
